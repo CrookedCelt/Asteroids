69bc99447979cd8f79dd4a2730b4fc1711170f42
crookedcelt <crookedcelt@gmail.com>
2024-09-21 16:29:29 -0400

    Added Infinite Game-loop and Close window feature

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import pygame
 from constants import *
+
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
