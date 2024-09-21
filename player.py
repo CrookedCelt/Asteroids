4366b752c405c69ae236879ff7087b5fa1d2b057
crookedcelt <crookedcelt@gmail.com>
2024-09-21 18:36:19 -0400

    Added Turn-speed and Update method so A and D keys Rotate Player

--- player.py
+++ player.py
@@ -1,4 +1,4 @@
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
 from circleshape import *
 
 class Player(CircleShape):
@@ -17,3 +17,15 @@ class Player(CircleShape):
     def draw (self, screen):
         super().draw(screen)
         pygame.draw.polygon (screen, "white", self.triangle(), 2)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:  # left
+            self.rotate(-dt)
+            
+        if keys[pygame.K_d]:  # right
+            self.rotate(dt)
