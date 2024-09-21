e708b90332c8b89e71afc9be3857a2efc29d6109
crookedcelt <crookedcelt@gmail.com>
2024-09-21 19:05:09 -0400

    Added Move Method for W and S keys

--- player.py
+++ player.py
@@ -1,4 +1,4 @@
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 from circleshape import *
 
 class Player(CircleShape):
@@ -21,11 +21,20 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
 
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:  # move up
+            self.move(dt)
 
-        if keys[pygame.K_a]:  # left
+        if keys[pygame.K_s]:  # move down
+            self.move(-dt)
+
+        if keys[pygame.K_a]:  # rotate left
             self.rotate(-dt)
-            
-        if keys[pygame.K_d]:  # right
+
+        if keys[pygame.K_d]:  # rotate right
             self.rotate(dt)
