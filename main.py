123872fd4aba7a5ef0776764f47787713182d87b
crookedcelt <crookedcelt@gmail.com>
2024-09-21 18:12:19 -0400

    Created Triangle player model and draw to screen

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
@@ -12,9 +15,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
             
-        screen.fill((0, 0, 0))
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-
         dt = clock.tick(60) / 1000
 
     print("Starting asteroids!")
