9cefc2a452222220e10ffd23c6793e0622213843
crookedcelt <crookedcelt@gmail.com>
2024-09-22 16:39:41 -0400

    Added updatable and drawable groups to main.py, added container to Player Class for both groups, now calling groups in loop

--- main.py
+++ main.py
@@ -4,19 +4,31 @@ from player import Player
 
 def main():
     pygame.init()
+
     clock = pygame.time.Clock()
     dt = 0
 
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    updatable_group = pygame.sprite.Group()
+    drawable_group = pygame.sprite.Group()
 
+    Player.containers = (updatable_group, drawable_group)
+    
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)    
+            
+        for updatable in updatable_group:
+            updatable.update(dt)  
+
         screen.fill("black")
-        player.draw(screen)
+
+        for drawable in drawable_group:
+            drawable.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
